95ef675b84ae81b13c37181fc3beabe5bd384269
helenhihi <hrparka@naver.com>
2022-02-12 21:51:07 +0900

    Create rock-scissors-paper.js

---
+++ js/rock-scissors-paper.js
@@ -0,0 +1,8 @@
+const computer = document.querySelector("#computer");
+const scissors = document.querySelector("#scissors");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const score = document.querySelector("#score");
+const IMG_URL = "./image/rsp.png";
+computer.style.background = `url(${IMG_URL})`;
+computer.style.backgroundSize = "auto 200px";
